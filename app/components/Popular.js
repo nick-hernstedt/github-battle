09923e6d7c770bf3c3cb6d14651d42f3c8906a97
copiousamounts <nick.hernstedt@gmail.com>
2020-05-05 13:45:23 -0700

    rendered button for each language

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,15 @@ import React from "react";
 
 export default class Popular extends React.Component {
   render() {
-    return <ul>POPULAR</ul>;
+    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    return (
+      <ul className="flex-center">
+        {languages.map((language, index) => (
+          <li key={language}>
+            <button className="btn-clear nav-link">{language}</button>
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ import Popular from "./components/Popular";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <Popular />
       </div>
     );
